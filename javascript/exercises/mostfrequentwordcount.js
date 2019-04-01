b31e01802d5018670e0b947777e8822ed4320fb2
alannarisse <angrypirate@gmail.com>
2019-03-31 22:45:40 -0700

    mostfreqword added

---
+++ javascript/exercises/mostfrequentwordcount.js
@@ -0,0 +1,12 @@
+Write a function to find count of the most frequent item of an array.
+
+const myArray = [1, 4, 6, 7, 8, 9, 0]
+
+function mostFreqItem(){
+
+}
+
+
+Write a function that accepts an array of 10 integers (between 0 and
+9), that returns a string of those numbers in the form of a phone
+number.
